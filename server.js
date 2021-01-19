be55d29258895e4fb9d1f897f6f4902bbe5180e7
Aayush Shah <shahaayush265@gmail.com>
2021-01-19 15:06:47 +0530

    Add files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const CardsChain = [
+    {"makeupimage":3,"theme":"image","social_media":{"facebook":"https://www.facebook.com/amar.mane.7330","insta":"https://www.instagram.com/amar.mane.007/"},"about":{"specialities":[],"description":[],"nature":"Furniture"},"gallery":{"slider":[],"photos":[]},"products":{"items":[]},"payment":{},"bank":{},"firstname":"Amar","Salutation":"Mr.","lastname":"Mane","company":"","img":"bubbly-blue","logo":"","email":"amarmane6907@gmail.com","phone":"+919326891093","whatsapp":"919326891093","map":"d","post":"Owner","vcf":"g"}
+]
+
+const app = express()
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json(CardsChain);
+})
+
+app.listen(process.env.PORT || '4200');
